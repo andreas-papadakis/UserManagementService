129a7d98b02e8f1af6d08452cf5a3142b3c9f9af
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-08-26 03:16:32 +0300

    Change all fields to private final using value annotation. Create private no args constructor for the needs of JpaRepository. Annotate the other constructor as json creator to be used when posting. Minor edits to fields assignments in constructor.

--- src/main/java/com/agileactors/user_management_service/model/User.java
+++ src/main/java/com/agileactors/user_management_service/model/User.java
@@ -1,9 +1,9 @@
 package com.agileactors.user_management_service.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -15,52 +15,60 @@ import java.time.LocalDateTime;
 
 @Entity
 @Table(name="users")
-@Getter
-@Setter
+@Value
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     @Schema(accessMode = Schema.AccessMode.READ_ONLY, example = "1", description = "User's unique identifier")
-    private Long id;
+    //TODO: replace with UUID and correct it in constructor
+    Long id;
 
     @Column(name = "first_name")
     @NotBlank(message = "First name must not be blank")
     @Size(min = 1, max = 100, message = "First name's length must not exceed 100 characters.")
     @Schema(example = "John", description = "User's first name")
-    private String firstName;
+    String firstName;
 
     @Column(name = "last_name")
     @NotBlank(message = "Last name must not be blank")
     @Size(min = 1, max = 100, message = "Last name's length must not exceed 100 characters.")
     @Schema(example = "Doe", description = "User's last name")
-    private String lastName;
+    String lastName;
 
     @Column(name = "email")
     @NotBlank(message = "e-mail must not be blank and in correct format.")
     @Size(min = 5, max = 100, message = "e-mail's length must not exceed 100 characters.")
     @Schema(name = "email", example = "johndoe@gmail.com", description = "User's e-mail")
-    private String email;
+    String email;
 
     @Column(name = "created_at")
     @CreationTimestamp
-    private LocalDateTime createdAt;
+    LocalDateTime createdAt;
 
     @Column(name = "updated_at")
     @UpdateTimestamp
-    private LocalDateTime updatedAt;
+    LocalDateTime updatedAt;
 
-    public String toString() {
-        return "ID: " + id + "\nFirst name: " + firstName + "\nLast name: " + lastName + "\ne-mail: " + email + "\n";
+    /**
+     * Private constructor used only for JpaRepository's reflection
+     */
+    private User() {
+        id        = null;
+        firstName = null;
+        lastName  = null;
+        email     = null;
+        createdAt = null;
+        updatedAt = null;
     }
 
+    @JsonCreator
     public User(String firstName, String lastName, String email) {
-        this.firstName = firstName.trim();
-        this.lastName = lastName.trim();
-        if(email.contains("@"))
-            if(email.substring(email.indexOf('@')).contains(".") )
-                this.email = email.trim();
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = createdAt;
+        this.id = 300L; //not important. Right now DB sets the correct one and will be updated to UUID instead of long
+        this.firstName = (firstName != null) ? firstName.trim() : null;
+        this.lastName  = (lastName != null) ? lastName.trim() : null;
+        this.email     = (email != null && email.contains("@") && email.substring(email.indexOf('@')).contains(".")) ? email.trim() : null;
+        this.createdAt = null; //not important; fixed by annotations
+        this.updatedAt = null; //not important; fixed by annotations
     }
 }
