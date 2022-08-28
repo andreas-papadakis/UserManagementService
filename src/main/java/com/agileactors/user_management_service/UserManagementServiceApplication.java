48bee5fe621946643fb4bbb44d82e56356d9bab9
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-08-28 05:16:22 +0300

    Update documentation

--- src/main/java/com/agileactors/user_management_service/UserManagementServiceApplication.java
+++ src/main/java/com/agileactors/user_management_service/UserManagementServiceApplication.java
@@ -17,10 +17,12 @@ public class UserManagementServiceApplication {
 		return new OpenAPI().info(new Info().title("User Management API")
 											 .description("Simple API with CRUD operations on MySQL DB running on docker. " +
 													 	  "The DB contains a single table called users with columns " +
-													 	  "id (type int, primary key, not null, auto increment), " +
-													      "first_name (type varchar(100), not null), " +
-													      "last_name (type varchar(100), not null) " +
-													 	  "and email (type varchar(100), not null)")
+													 	  "id (type CHAR(36), primary key, not null), " +
+													      "first_name (type VARCHAR(100), not null), " +
+													      "last_name (type VARCHAR(100), not null), " +
+													 	  "email (type VARCHAR(100), not null), " +
+											 			  "created_at (type DATETIME, not null, " +
+													 	  "and updated_at (type DATETIME).")
 								 );
 	}
 }
