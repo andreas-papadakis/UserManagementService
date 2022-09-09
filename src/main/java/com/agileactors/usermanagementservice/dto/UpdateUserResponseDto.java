b6a28f6122042b8352df76c8ec1375c26b6f4fe0
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-09-10 02:12:07 +0300

    Change id from string to UUID.

--- src/main/java/com/agileactors/usermanagementservice/dto/UpdateUserResponseDto.java
+++ src/main/java/com/agileactors/usermanagementservice/dto/UpdateUserResponseDto.java
@@ -3,8 +3,9 @@ package com.agileactors.usermanagementservice.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record UpdateUserResponseDto(@Schema(name = "id", example = "123e4567-e89b-12d3-a456-426614174000", description = "User's unique identifier") String id,
+public record UpdateUserResponseDto(@Schema(name = "id", example = "123e4567-e89b-12d3-a456-426614174000", description = "User's unique identifier") UUID id,
                                     @Schema(name = "first_name", example = "John", description = "User's first name") String firstName,
                                     @Schema(name = "last_name", example = "Doe", description = "User's last name") String lastName,
                                     @Schema(name = "email", example = "johndoe@gmail.com", description = "User's e-mail") String email,
