dc732fd91ec0ff546585e9afadb32bfe60d6c276
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-09-11 02:55:19 +0300

    Remove schema annotations as User class is not one anymore.

--- src/main/java/com/agileactors/usermanagementservice/model/User.java
+++ src/main/java/com/agileactors/usermanagementservice/model/User.java
@@ -1,7 +1,5 @@
 package com.agileactors.usermanagementservice.model;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -18,19 +16,15 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @Column(name = "id")
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY, example = "1", description = "User's unique identifier")
     String id;
 
     @Column(name = "first_name")
-    @Schema(example = "John", description = "User's first name")
     String firstName;
 
     @Column(name = "last_name")
-    @Schema(example = "Doe", description = "User's last name")
     String lastName;
 
     @Column(name = "email")
-    @Schema(name = "email", example = "johndoe@gmail.com", description = "User's e-mail")
     String email;
 
     @Column(name = "created_at")
