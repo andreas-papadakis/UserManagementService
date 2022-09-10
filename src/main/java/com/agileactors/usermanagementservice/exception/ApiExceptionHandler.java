ff79d3903701b74ffb4584a191c5d1d542ff400e
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-09-11 01:43:08 +0300

    Create custom exception handler.

---
+++ src/main/java/com/agileactors/usermanagementservice/exception/ApiExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.agileactors.usermanagementservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler (value = UserNotFoundException.class)
+    public ResponseEntity<ApiException> handleUserNotFoundException(UserNotFoundException userNotFoundException) {
+        return new ResponseEntity<>(new ApiException(userNotFoundException.getMessage(),
+                                                     HttpStatus.NOT_FOUND,
+                                                     LocalDateTime.now()),
+                                    HttpStatus.NOT_FOUND);
+    }
+}
