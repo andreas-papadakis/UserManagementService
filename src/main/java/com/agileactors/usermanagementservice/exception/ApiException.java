528aa58bf85c6219e34284e0cbe44a2702eff827
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-09-11 02:02:19 +0300

    Add swagger documentation.

--- src/main/java/com/agileactors/usermanagementservice/exception/ApiException.java
+++ src/main/java/com/agileactors/usermanagementservice/exception/ApiException.java
@@ -1,7 +1,22 @@
 package com.agileactors.usermanagementservice.exception;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
-public record ApiException(String message, HttpStatus httpStatus, LocalDateTime timestamp) { }
+public record ApiException(@Schema (name = "message",
+                                    example = "User with ID 123e4567-e89b-12d3-a456-426614174000 does not exist",
+                                    description = "Message providing detailed info about occurred error")
+                           String message,
+
+                           @Schema (name = "httpStatus",
+                                   example = "404 NOT_FOUND",
+                                   description = "The http status code of the occurred error",
+                                   type = "string")
+                           HttpStatus httpStatus,
+
+                           @Schema (name = "timestamp",
+                                   example = "2022-09-10T22:51:20.879Z",
+                                   description = "The time the error occurred")
+                           LocalDateTime timestamp) { }
