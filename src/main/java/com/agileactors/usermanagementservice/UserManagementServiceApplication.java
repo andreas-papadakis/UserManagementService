e73ea92fbe4c2811e9fd12fa41101fcf1d223269
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-09-14 07:02:04 +0300

    Comply with Google coding style.

--- src/main/java/com/agileactors/usermanagementservice/UserManagementServiceApplication.java
+++ src/main/java/com/agileactors/usermanagementservice/UserManagementServiceApplication.java
@@ -2,27 +2,40 @@ package com.agileactors.usermanagementservice;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * Main class of API.
+ */
+@SuppressWarnings("checkstyle:LineLengthCheck")
 @SpringBootApplication
 public class UserManagementServiceApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(UserManagementServiceApplication.class, args);
-	}
+  /**
+   * Main method of API.
+   *
+   * @param args Arguments passed to program
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(UserManagementServiceApplication.class, args);
+  }
 
-	public @Bean OpenAPI userManagementAPI() {
-		return new OpenAPI().info(new Info().title("User Management API")
-											 .description("Simple API with CRUD operations on MySQL DB running on docker. " +
-													 	  "The DB contains a single table called users with columns " +
-													 	  "id (type CHAR(36), primary key, not null), " +
-													      "first_name (type VARCHAR(100), not null), " +
-													      "last_name (type VARCHAR(100), not null), " +
-													 	  "email (type VARCHAR(100), not null), " +
-											 			  "created_at (type DATETIME, not null, " +
-													 	  "and updated_at (type DATETIME).")
-								 );
-	}
+  /**
+   * Build swagger description in web page.
+   *
+   * @return Swagger OpenAPI
+   */
+  public @Bean OpenAPI userManagementApi() {
+    return new OpenAPI().info(new Info().title("User Management API")
+                                        .description("Simple API with CRUD operations on MySQL DB running on docker. "
+                                                   + "The DB contains a single table called users with columns "
+                                                   + "id (type CHAR(36), primary key, not null), "
+                                                   + "first_name (type VARCHAR(100), not null), "
+                                                   + "last_name (type VARCHAR(100), not null), "
+                                                   + "email (type VARCHAR(100), not null), "
+                                                   + "created_at (type DATETIME, not null, "
+                                                   + "and updated_at (type DATETIME).")
+                             );
+  }
 }
