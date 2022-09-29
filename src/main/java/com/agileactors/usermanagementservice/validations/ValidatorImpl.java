cacbffcbbe2ff415e939b8a2d18b4f718a33db5d
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-09-29 23:10:04 +0300

    Remove suppress missing javadoc type warning from checkstyle and create javadoc for class and inherit from interface for the method.

--- src/main/java/com/agileactors/usermanagementservice/validations/ValidatorImpl.java
+++ src/main/java/com/agileactors/usermanagementservice/validations/ValidatorImpl.java
@@ -3,10 +3,15 @@ package com.agileactors.usermanagementservice.validations;
 import com.agileactors.usermanagementservice.exception.InvalidArgumentException;
 import org.springframework.stereotype.Component;
 
-@SuppressWarnings("checkstyle:MissingJavadocType")
+/**
+ * Class implementing the {@link com.agileactors.usermanagementservice.validations.Validator}
+ * interface.
+ */
 @Component
 public class ValidatorImpl implements Validator {
-  @SuppressWarnings("checkstyle:MissingJavadocMethod")
+  /**
+   * {@inheritDoc}
+   */
   public void validateEmail(String email) {
     if (!email.matches("[a-zA-Z0-9]+@[a-zA-Z]+[.][a-zA-Z]+")) {
       throw new InvalidArgumentException("Invalid email.");
