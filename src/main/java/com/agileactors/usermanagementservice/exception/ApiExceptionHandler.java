f6b41fd4f2fd1114f1582a8dac64f6ae900cee59
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-10-02 05:12:00 +0300

    Create new method to handle EmptyResultDataAccessException.

--- src/main/java/com/agileactors/usermanagementservice/exception/ApiExceptionHandler.java
+++ src/main/java/com/agileactors/usermanagementservice/exception/ApiExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.agileactors.usermanagementservice.exception;
 
 import java.time.LocalDateTime;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -54,16 +55,34 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
    * @param illegalArgumentException The exception
    *
    * @return {@link ResponseEntity} of type
-   *     {@link com.agileactors.usermanagementservice.exception.ApiException} with standard message,
-   *     500 HTTP status and the time it occurred
+   *         {@link com.agileactors.usermanagementservice.exception.ApiException} with standard
+   *         message, 500 HTTP status and the time it occurred
    */
   @ExceptionHandler (value = IllegalArgumentException.class)
   public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException
                                                                  illegalArgumentException) {
-    System.err.println(illegalArgumentException.getMessage());
+    System.err.println(illegalArgumentException.getMessage()); //TODO: remove
     return new ResponseEntity<>(new ApiException("Internal server error",
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            LocalDateTime.now()),
-            HttpStatus.INTERNAL_SERVER_ERROR);
+                                                 HttpStatus.INTERNAL_SERVER_ERROR,
+                                                 LocalDateTime.now()),
+                                HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  /**
+   * Handle {@link EmptyResultDataAccessException}.
+   *
+   * @param emptyResultDataAccessException The exception
+   *
+   * @return {@link ResponseEntity} of type
+   *         {@link com.agileactors.usermanagementservice.exception.ApiException} with the
+   *         exception's message, 404 HTTP status and the time it occurred
+   */
+  @ExceptionHandler (value = EmptyResultDataAccessException.class)
+  public ResponseEntity<Object> handleEmptyResultDataAccessException(
+          EmptyResultDataAccessException emptyResultDataAccessException) {
+    return new ResponseEntity<>(new ApiException(emptyResultDataAccessException.getMessage(),
+                                                 HttpStatus.NOT_FOUND,
+                                                 LocalDateTime.now()),
+                                HttpStatus.NOT_FOUND);
   }
 }
