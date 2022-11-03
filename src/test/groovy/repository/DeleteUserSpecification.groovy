86b4c32de1f1e9eb4e1a3c1fa97e8eaebc22b9c0
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-11-04 00:32:41 +0200

    Create specification for deletion of single user.

--- src/test/groovy/repository/DeleteUserSpecification.groovy
+++ src/test/groovy/repository/DeleteUserSpecification.groovy
@@ -1,10 +1,12 @@
 package repository
 
 import com.agileactors.usermanagementservice.UserManagementServiceApplication
+import com.agileactors.usermanagementservice.model.User
 import com.agileactors.usermanagementservice.repository.UserRepository
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+
 import spock.lang.Specification
 import spock.lang.Subject
 
@@ -21,5 +23,21 @@ class DeleteUserSpecification extends Specification {
     userRepository.deleteAll()
   }
 
+  def "should delete user"() {
+    given: "a new user is created"
+    User newUser = new User(UUID.randomUUID(),
+                            "testFName",
+                            "testLName",
+                            "a@a.com",
+                            null,
+                            null);
+    and: "saved in DB"
+    userRepository.save(newUser)
 
+    when: "the user is deleted"
+    userRepository.delete(newUser)
+
+    then: "user can no longer be retrieved"
+    !userRepository.findById(newUser.id).isPresent()
+  }
 }
