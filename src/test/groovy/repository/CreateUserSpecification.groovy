2083e88bb601946f30935ca4fd014beecee3e5c7
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-11-03 23:51:55 +0200

    Create method to test user validations.

--- src/test/groovy/repository/CreateUserSpecification.groovy
+++ src/test/groovy/repository/CreateUserSpecification.groovy
@@ -1,9 +1,13 @@
 package repository
 
+import com.agileactors.usermanagementservice.dto.CreateUserRequestDto
 import com.agileactors.usermanagementservice.model.User
 import com.agileactors.usermanagementservice.repository.UserRepository
 import com.agileactors.usermanagementservice.UserManagementServiceApplication
 
+import javax.validation.Validation
+
+import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -32,5 +36,31 @@ class CreateUserSpecification extends Specification {
     userRepository.findById(newUser.getId()).isPresent()
   }
 
+  def "#user should fail to pass validation"() {
+    expect: "user failed to pass validation"
+    !Validation.buildDefaultValidatorFactory().validator.validate(user).empty
 
+    where: "user has either blank first/last name and or e-mail and/or has more than 100 chars in first/last name and/or e-mail"
+    user << [new CreateUserRequestDto("",
+                                      "testLName",
+                                      "a@a.com"),
+             new CreateUserRequestDto("testFname",
+                                      "",
+                                      "a@a.com"),
+             new CreateUserRequestDto(RandomStringUtils.randomAlphabetic(101),
+                                      "testLName",
+                                      "a@a.com"),
+             new CreateUserRequestDto("lala",
+                                      " ",
+                                      "a@a.com"),
+             new CreateUserRequestDto("lala",
+                                      RandomStringUtils.randomAlphabetic(101),
+                                      "a@a.com"),
+             new CreateUserRequestDto("lala",
+                                      "lala",
+                                      " "),
+             new CreateUserRequestDto("lala",
+                                      "lala",
+                                      RandomStringUtils.randomAlphabetic(101))]
+  }
 }
