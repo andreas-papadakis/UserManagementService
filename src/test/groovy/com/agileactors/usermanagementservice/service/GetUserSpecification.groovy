6824dba3e1cbab658ee8b4bc78174e252dcdd5e8
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-11-06 18:37:45 +0200

    Create specification to test getAllUsers method from service layer.

---
+++ src/test/groovy/com/agileactors/usermanagementservice/service/GetUserSpecification.groovy
@@ -0,0 +1,44 @@
+package com.agileactors.usermanagementservice.service
+
+import com.agileactors.usermanagementservice.model.GetUserModel
+import com.agileactors.usermanagementservice.model.User
+import com.agileactors.usermanagementservice.repository.UserRepository
+import com.agileactors.usermanagementservice.validations.Validator
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.core.convert.ConversionService
+import spock.lang.Specification
+import spock.lang.Subject
+
+@SpringBootTest
+class GetUserSpecification extends Specification {
+  private UserRepository userRepository       = Mock()
+  private Validator validator                 = Mock()
+  private ConversionService conversionService = Mock()
+  @Subject
+  private UserServiceImpl userService
+
+  /**
+   * Setup user service before any specification
+   */
+  def setup() {
+    userService = new UserServiceImpl(userRepository, validator, conversionService)
+  }
+
+  def "should get all users"() {
+    given: "a request to retrieve all users from DB arrived"
+    GetUserModel getUserModel = new GetUserModel(null, null)
+
+    when: "service method to retrieve the users is called"
+    userService.getAllUsers(getUserModel)
+
+    then: "service layer calls only findAll method from repository"
+    1 * userRepository.findAll() >> new ArrayList<User>()
+    0 * userRepository.findByFirstNameLike(_ as String)
+    0 * userRepository.findByLastNameLike(_ as String)
+    0 * userRepository.findByFirstNameLikeAndLastNameLike(_ as String, _ as String)
+  }
+
+  def cleanup() {
+    userService = null
+  }
+}
