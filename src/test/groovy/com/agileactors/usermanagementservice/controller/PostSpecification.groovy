ac446f4df6ae29001c8a3002c277e459ca3ca292
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-11-09 20:14:02 +0200

    Implement test on invalid CreateUserRequestDto.

--- src/test/groovy/com/agileactors/usermanagementservice/controller/PostSpecification.groovy
+++ src/test/groovy/com/agileactors/usermanagementservice/controller/PostSpecification.groovy
@@ -1,9 +1,16 @@
 package com.agileactors.usermanagementservice.controller
 
+import com.agileactors.usermanagementservice.exception.ApiException
+import com.agileactors.usermanagementservice.exception.ApiExceptionHandler
+import org.springframework.http.HttpStatus
+import org.springframework.web.util.NestedServletException
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 import com.agileactors.usermanagementservice.dto.CreateUserRequestDto
 import com.agileactors.usermanagementservice.dto.CreateUserResponseDto
+import com.agileactors.usermanagementservice.exception.InvalidArgumentException
 import com.agileactors.usermanagementservice.model.User
 import com.agileactors.usermanagementservice.service.UserService
 import com.agileactors.usermanagementservice.UserManagementServiceApplication
@@ -32,7 +39,9 @@ class PostSpecification extends Specification {
    */
   def setup() {
     userController = new UserController(userService, conversionService)
-    mockMvc        = MockMvcBuilders.standaloneSetup(userController).build()
+    mockMvc        = MockMvcBuilders.standaloneSetup(userController)
+                                    .setControllerAdvice(ApiExceptionHandler.class)
+                                    .build()
   }
 
   def "on valid create user request, controller should create a user and return Http status 200"() {
@@ -48,13 +57,33 @@ class PostSpecification extends Specification {
                                            .andReturn()
 
     then: "Http status of 200 is returned"
-    result.getResponse().status == 200
+    result.getResponse().status == HttpStatus.OK.value()
 
     and: "createUser from service layer was called once"
     1 * userService.createUser(_ as CreateUserRequestDto) >> new User()
 
     and: "controller converts the returned user from createUser to CreateUserResponseDto"
     1 * conversionService.convert(_ as User, CreateUserResponseDto.class)
+  }
+
+  def "on invalid create user request, controller should throw an exception and exception handler return http status of 400"() {
+    given: "an invalid createUserRequestDto"
+    CreateUserRequestDto createUserRequestDto = new CreateUserRequestDto("",
+                                                                         "lastName",
+                                                                         "a@a.com")
+
+    when: "controller executes the post request"
+    MvcResult result = mockMvc.perform(post("/api/users")
+                   .contentType("application/json")
+                   .content(objectMapper
+                              .writeValueAsString(createUserRequestDto)))
+                   .andReturn()
+
+    then: "an InvalidArgumentException was thrown and exception handler caught it"
+    result.getResolvedException() instanceof InvalidArgumentException
+
+    and: "it set an http status of 400"
+    result.getResponse().status == HttpStatus.BAD_REQUEST.value()
   }
 
   /**
