b8dd3b6893862c9ef85ceeec093b85d2bd63af3f
andreas-papadakis <andreas.apo.ftw@gmail.com>
2022-12-10 17:47:32 +0200

    Check if email is null.

--- src/main/java/com/agileactors/usermanagementservice/validations/ValidatorImpl.java
+++ src/main/java/com/agileactors/usermanagementservice/validations/ValidatorImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 class ValidatorImpl implements Validator { //TODO: remove public modifier
   /** {@inheritDoc} */
   public void validateEmail(String email) throws InvalidArgumentException {
-    if (!email.matches("[a-zA-Z0-9]+@[a-zA-Z]+[.][a-zA-Z]+")) { //TODO: check if string is null
+    if (email == null || !email.matches("[a-zA-Z0-9]+@[a-zA-Z]+[.][a-zA-Z]+")) {
       throw new InvalidArgumentException("Invalid email.");
     }
   }
